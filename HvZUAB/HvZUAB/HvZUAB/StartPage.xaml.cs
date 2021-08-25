04f0c8ea1ccbd4be5d2ab71d1694125406aba9ac
darkb <darkb@DESKTOP-6OQ3J3V>
2021-08-25 18:24:22 +0200

    first commit

---
+++ HvZUAB/HvZUAB/HvZUAB/StartPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HvZUAB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        public void RegisterNavClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+        public void LogInNavClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
